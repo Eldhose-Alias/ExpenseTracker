d1154bee82f9060b8e9c74d4d1f9a4524be6cde3
unknown <eldhosealias555@gmail.com>
2023-02-15 11:23:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import HomeComponent from './home/Index';
+import Overview from './home/Overview';
+import Transacttion from './home/Transacttion';
 
+const Container=styled.div`
+display:flex;
+flex-direction:column;
+font-family:Montserrat;
+align-items: center;
+margin: 30px 0 10px;
+`
+const Headers=styled.div`
+font-size:25px;
+font-weight:bold;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Headers>Expense Tracker</Headers>
+      <HomeComponent/>
+      <Overview/>
+      <Transacttion/>
+    </Container>
   );
 }
 
